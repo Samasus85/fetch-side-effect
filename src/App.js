2ba4e60d33ab963f46608cf52fe2c4b73c30b866
Samasus85 <soliha.karim@mail.ru>
2022-02-01 21:41:57 +0300

    I created app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import Posts from './components/Posts';
 function App() {
+  const [item, setItem] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=12')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => setItem([...data]));
+    setIsLoading(false);
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? <p>Loading...</p> : <Posts data={item} />}
     </div>
   );
 }
-
 export default App;
